fb34bea737dbc3cad753d4fc7f421b4718c8d899
steve2482 <stevecb12684@yahoo.com>
2016-12-28 22:26:42 -0500

    fixed issues with GET and POST reqests, added code for PUT request which is getting a 'Cannot PUT' error

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -15,10 +15,10 @@ BlogPosts.create(
 
 // send back JSON of all posts
 router.get('/', (req, res) => {
-  res.json(BlogPosts.get);
+  res.json(BlogPosts.get());
 });
 
-router.post('/', (req, res) => {
+router.post('/', jsonParser, (req, res) => {
   // check for required blog post fields in body of request
   const requiredFields = ['title', 'content', 'author'];
   for (let i = 0; i < requiredFields.length; i++) {
@@ -29,8 +29,30 @@ router.post('/', (req, res) => {
       return res.status(400).send(message);
     }
   }
-  const post = BlogPosts.create(req.body.title, req.body.content, req.body.authur);
+  const post = BlogPosts.create(req.body.title, req.body.content, req.body.author);
   res.status(201).json(post);
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['id', 'title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  console.log(`Updating blog post \`${req.params.id}\``);
+  const updatedPost = BlogPosts.update(
+    {
+      id: req.params.id,
+      title: req.params.title,
+      content: req.params.content,
+      author: req.params.author,
+      publishDate: req.params.publishDate
+    });
+  res.status(204).json(updatedPost);
 });
 
 module.exports = router;
