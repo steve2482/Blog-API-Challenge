61295085bacbae764a01bf54cc520fec5e4432d1
steve2482 <stevecb12684@yahoo.com>
2017-01-05 12:52:03 -0500

    folder name fix

---
+++ test/test-server.js
@@ -0,0 +1,48 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server')
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('BlogPosts', function() {
+
+	before(function() {
+		return runServer();
+	});
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should list blog posts on GET', function() {
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.above(0);
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.have.all.keys(
+            'id', 'title', 'content', 'author', 'publishDate');
+        });
+      });
+  });
+
+  // it('should return a post on POST', function() {
+  //   const newPost = {
+  //     'title': 'New Post',
+  //     'content': 'This is a new post',
+  //     'author': 'Test'
+  //   }
+  //   return chai.request(app)
+  //     .post('/blog-posts')
+  //     .then(function(res) {
+        
+  //     })
+  // })
+});
