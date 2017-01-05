ee8320dd6db7dd995916fe6b78465aa88a8e9f3d
steve2482 <stevecb12684@yahoo.com>
2017-01-05 12:11:20 -0500

    GET testing added

--- Test/test-server.js
+++ Test/test-server.js
@@ -18,17 +18,17 @@ describe('BlogPosts', function() {
   });
 
   it('should list blog posts on GET', function() {
-    return chai.request(server)
-      .get('/')
+    return chai.request(app)
+      .get('/blog-posts')
       .then(function(res) {
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.be.a('array');
-        res.body.length.should.be.atleast(1);
+        res.body.length.should.be.above(0);
         res.body.forEach(function(item) {
           item.should.be.a('object');
           item.should.have.all.keys(
-            'id', 'title', 'content', 'author', 'publisDate');
+            'id', 'title', 'content', 'author', 'publishDate');
         })
       })
   })
